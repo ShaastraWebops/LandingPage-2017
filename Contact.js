55508ff82fb2e916df6ad36fd697d9004ef6ef23
ojasvita <ojasvitawalgad@gmail.com>
2016-07-02 00:49:24 +0530

    Contact Details Added

---
+++ Contact.js
@@ -0,0 +1,127 @@
+var app=angular.module('App',[ ]);
+app.controller('ContactController', function(){
+this.contacts = cont_details;
+});
+var cont_details = [
+{
+		name:"CoCAS : Animesh Bagora",
+		Contact: 9884299639,
+		Email: "animesh.bagora@gmail.com",
+},
+{
+		name:"Concept & Design(1) : Pendse Neil Sanjay",
+		Contact:9176283794,
+		Email:"neil.pendse@gmail.com" ,
+},
+{
+		name:"Concept & Design(2) : Abhijit S Gupta",
+		Contact:9940439339,
+		Email:"abhijit.sri.gupta@gmail.com" ,
+},
+{
+		name:"Concept & Design(3) : Bharath",
+		Contact:9940579724,
+		Email:"ae15d019@smail.iitm.ac.in" ,
+},
+{
+		name:"Envisage(1) : Harshit Dohare ",
+		Contact:9962879064,
+		Email:"harshit13hd@gmail.com" ,
+},
+{
+		name:"Envisage(2) : Omkar Vanjpe",
+		Contact:7708173059,
+		Email:"omkarkvanjpe@gmail.com" ,
+},
+{
+		name:"Events(1) : Adarsh",
+		Contact:9600193202, 
+		Email:"1adarshg@gmail.com" ,
+},
+{
+		name:"Events(2) : Suvindu Ch",
+		Contact:9940248645,
+		Email:"suvindu.ch@gmail.com" ,
+},
+{
+		name:"Events(3) : Rohit Thakran",
+		Contact:9958515422,
+		Email:"7rohitthakran@gmail.com" ,
+},
+{
+		name:"Evolve(1) : Ramprashanth",
+		Contact:8754443260,
+		Email:"m.ramprashanth@gmail.com" ,
+},
+{
+		name:"Evolve(2) : Udith Krishna",
+		Contact:9551113873,
+		Email:"udithsamsung@gmail.com" ,
+},
+{
+		name:"Facilities : Harshit Saxena",
+		Contact:9789016167,
+		Email:"harshitsaxena1795@gmail.com" ,
+},
+{
+		name:"Finance(1) : Krishi Tata",
+		Contact:9003113121,
+		Email:"krishitata045@gmail.com" ,
+},
+{
+		name:"Finance(2) : L Sai Manish ",
+		Contact:7708481881,
+		Email:"saimanish12337@gmail.com" ,
+},
+{
+		name:"Shows and Exhibitions : Nivedita Manepalli",
+		Contact:9940557095,
+		Email:"niveditamanepalli@gmail.com" ,
+},
+{
+		name:"Sponsorship & PR(1) : Raghul Manosh",
+		Contact:9940246479,
+		Email:"raghulmanosh9@gmail.com" ,
+},
+{
+		name:"Sponsorship & PR(2) :Varun Srinivas Sridharan",
+		Contact:7358540260,
+		Email:"varun.srinivas.sridharan@gmail.com" ,
+},
+{
+		name:"Media & Student Relations(1) : M Aghamarshana",
+		Contact:8122495355,
+		Email:"aghu1995@gmail.com" ,
+},
+{
+		name:"Media & Student Relations(2) : R Rathna Kumar",
+		Contact:9677081113,
+		Email:"rathnakumarravichandran@gmail.com" ,
+},
+{
+		name:"Media & Student Relations(3) : Hitesh Singla",
+		Contact:9677043890,
+		Email:"hiteshsingla90@gmail.com" ,
+},
+{
+		name:"Technical Operations(1) : Mohit Kumar",
+		Contact:9003120598,
+		Email:"mhtkmr54@gmail.com" ,
+},
+{
+		name:"Technical Operations(2) : S.V.Minu Aswanth",
+		Contact:9976448668,
+		Email:"minuapple@gmail.com" ,
+},
+{
+		name:"QMS(1) : Manish Dhariwal",
+		Contact:9884184032,
+		Email:"manishdhariwal28@gmail.com" ,
+},
+{
+		name:"QMS(2) : Gunjan",
+		Contact:7299559744,
+		Email:"bmsw.gnp@gmail.com" ,
+}
+];
+
